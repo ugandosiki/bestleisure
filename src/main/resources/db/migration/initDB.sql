fa8186bcff7e37552eb6ceffbd9b151bbe43170e
JustEazy <vladkolomiets2012@gmail.com>
2021-03-07 12:28:22 +0300

    Update initDB.sql

--- src/main/resources/db/migration/initDB.sql
+++ src/main/resources/db/migration/initDB.sql
@@ -44,6 +44,7 @@ CREATE TABLE IF NOT EXISTS posts
     image_id       INTEGER,
     title          VARCHAR(250) UNIQUE NOT NULL,
     description    TEXT,
+    likes          INTEGER,
     created_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES categories (id),
     FOREIGN KEY (subCategory_id) REFERENCES subCategories (id),
